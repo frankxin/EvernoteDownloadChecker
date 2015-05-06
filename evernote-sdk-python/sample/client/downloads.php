917a4cdbbf1df05286ea1a12fb39355d3e0d0586
frankxin <frankxin93@hotmail.com>
2015-05-06 15:53:41 +0800

    familiar with base fun

---
+++ evernote-sdk-python/sample/client/downloads.php
@@ -0,0 +1,132 @@
+<?php
+// L10N_LIMIT_DESTINATION_LANGUAGES=en 
+
+$alldownloads = array (
+
+# EVERNOTE:
+
+	# Full Mac release (update "PrereleaseMac", too...)
+		"EvernoteMac"			=> "http://cdn1.evernote.com/mac-smd/public/Evernote_RELEASE_6.0.11_451580.dmg", // https://cdn1.evernote.com/mac/release/Evernote_402628.dmg  // direct download option; //jacks script https://update.evernote.com/download/?file=EvernoteMac
+		"EvernoteMacApp"		=> "http://appstore.com/mac/evernote/evernote/", // iTunes: http://itunes.apple.com/app/evernote/id406056744  http://appstore.com/mac/evernote/evernote/
+		"PrereleaseMac"			=> "https://cdn1.evernote.com/mac-smd/prerelease/Evernote_BETA_6.0.9_Beta_451460.zip", // Mac pre-releases
+		"EvernoteMacLegacy"		=> "https://cdn1.evernote.com/mac/release/Evernote_300201.dmg", // 3.3 - keep for Support folks, etc.
+		"EvernoteMacSnow"   => "https://cdn1.evernote.com/mac/release/Evernote_402634.dmg", // old snow leopard download
+		"MacClipperExtension"	=> "https://update.evernote.com/public/ENMac/SafariExtension/Evernote.1.0.2.safariextz",
+		"MacUnsupported"		=> "https://cdn1.evernote.com/mac/release/Evernote_158341.dmg",
+
+	# Full Windows release
+		"Win"					=> "http://cdn1.evernote.com/win5/public/Evernote_5.8.6.7519.exe",
+		
+	# Current Windows pre-release
+		"PrereleaseWinBeta"  => 	"https://cdn1.evernote.com/win5/prerelease/Evernote_5.8.4.6736.exe",
+
+	# Windows 5 Beta
+		"Win5Beta"				=> "https://cdn1.evernote.com/win5/prerelease/Evernote_5.2.0.2503.exe",
+
+	# Full Windows release, version 4.6
+  		"Win46"					=> 'https://cdn1.evernote.com/win4/public/Evernote_4.6.7.8409.exe',
+
+	# Full Windows release, version 3.5
+		"Win35"					=> "https://cdn1.evernote.com/win/Evernote_3.5.6.2848.exe",
+	
+	# Full Windows release, version 3.1
+		"Win31"					=> "https://cdn1.evernote.com/Evernote_3.1.0.1225.exe",
+	
+	# Windows Mobile client files
+		"EvernoteWMexe"			=> "https://cdn1.evernote.com/EvernoteWM_3.3_64861.exe",
+		"EvernoteWMcab"			=> "https://cdn1.evernote.com/EvernoteWM_3.3_64861.cab",
+	
+	# Special Palm WM, used in an email sent by Palm Q4"08, remove eventually
+		"EvernotePalmWMexe"		=> "https://cdn1.evernote.com/EvernoteWM_3.3_42128_palm.exe",
+		"EvernotePalmWMcab"		=> "https://cdn1.evernote.com/EvernoteWM_3.3_42128_palm.cab",
+	
+	# WM build for Sony-Ericsson X1 phones (with Panels)
+		"EvernoteX1"			=> "https://cdn1.evernote.com/EvernoteWM_SEPanel_3.3_47072.cab",
+	
+	# Android Releases
+		"Android"				=> "https://cdn1.evernote.com/android/public/com.evernote.public.AM_611.allArch.96.apk",
+	
+	# Android pre-releases
+		"PrereleaseAndroid"		=> "https://cdn1.evernote.com/android/beta/com.evernote.beta.master.all.1014.apk",
+	
+	# Android Widget Releases
+		"AndWidget"				=> "https://cdn1.evernote.com/android/widget/public/com.evernote.widget.public.AM_641.552.apk",
+	
+	# Android Widget Pre-releases
+		"PreAndWidget"			=> "https://cdn1.evernote.com/android/widget/beta/com.evernote.widget.beta.master.512.apk",
+	
+	# Evernote Android Releases FOR CHINA
+		"AndroidYinxiangCom"	=> "https://cdn1.evernote.com/android/yinxiang/com.evernote.yinxiang.AM_703.allArch.589.apk", //Com apk 
+	
+	# Blackberry downloads in different forms
+		"BBzip"					=> "https://update.evernote.com/public/ENBB/EvernoteBB_3.0.249.zip",
+		"BBStormzip"			=> "https://update.evernote.com/public/ENBB-Storm/EvernoteBBStorm_3.0.249.zip",
+		"BBjad"					=> "https://update.evernote.com/public/ENBB/torch_6.0.0/Evernote.jad",
+		"BBStormjad"			=> "https://update.evernote.com/public/ENBB-Storm/Evernote.jad",
+	
+	# Nokia Web Runtime
+		"PrereleaseNokiaWRT"	=> "https://update.evernote.com/internal/ENWRT/Evernote_70302.wgz",
+	
+# WEBCLIPPER:
+	
+	# Ancient clipper for Firefox 3 and EN 2.2
+		"ENWebClipper15"		=> "https://cdn1.evernote.com/win/2.2/firefox/webclipper-2.0.0.122-fx-tb-windows.xpi",
+	
+	# Safari extension download
+		"SafariExtension"		=> "https://cdn1.evernote.com/mac/safari_clipper/Evernote.6.4.safariextz",
+	
+	#direct download link for YXBJ
+		"ClipperChromeYXBJ"		=> "https://cdn1.evernote.com/yinxiang/webclipper/chrome/chrome-ext-6.3.crx",
+
+# Scannable
+		"Scannable"		=> "https://itunes.apple.com/us/app/evernote-scannable/id883338188",
+		"ScannableIOS"		=> "https://itunes.apple.com/us/app/evernote-scannable/id883338188",
+	
+# CLEARLY:
+	
+	#direct download link for YXBJ
+		"ClearlyChromeYXBJ"		=> "https://cdn1.evernote.com/yinxiang/clearly/chrome/clearly_10_4_1_6.crx",
+		"ClearlyFirefoxYXBJ"	=> "https://cdn1.evernote.com/yinxiang/clearly/firefox/Clearly_93x_firefox.xpi",
+	
+	
+# SKITCH:
+	# Full Mac release
+		"SkitchMac"				=> "https://cdn1.evernote.com/skitch/mac/internal/Skitch-2.7.6.zip",
+		"SkitchMacApp"			=> "http://appstore.com/mac/skitch/skitch/",
+		
+	# Full Windows release
+		"SkitchWindows"			=> "https://cdn1.evernote.com/Skitch/win32/skitchsetup-2.3.2.173.exe",
+
+	# Android release
+		"SkitchAndroidApp"		=> "https://play.google.com/store/apps/details?id=com.evernote.skitch",
+	
+	# Skitch v1 for MAC:
+		"SkitchMac_v1"			=> "http://get.skitch.com/Skitch-1.0.12.zip",
+		"SkitchMac_v1.0"		=> "https://cdn1.evernote.com/android/yinxiang/Skitch-1.0.12.zip",
+	
+	# SKITCH for Android on YINXIANG:
+		"SkitchAndroid"			=> "https://cdn1.evernote.com/yinxiang/skitch/android/skitch-gradle-allArch-release-40-yinxiang.apk",
+
+# HELLO:
+	
+	# Android on YINXIANG:
+		"HelloAndroidYXBJ"		=> "https://cdn1.evernote.com/android/hello-delivery/yinxiang/Hello_yinxiang.v1.4.2rc1.78.apk",
+
+# FOOD:
+
+	# Android on YINXIANG:  
+		"FoodAndroidYXBJ"		=> "https://cdn1.evernote.com/yinxiang/food/android/Food_Android_Business_Delivery_yinxiang.v2.0.6_94.apk",
+
+# SCANSNAP DRIVERS:
+
+	# ScanSnap for Mac:
+		"ScanSnapMac"			=> "https://cdn1.evernote.com/scansnap/mac/ScanSnap_Evernote_Edition_Installer_196.dmg",
+
+	# ScanSnap for Windows:
+		"ScanSnapWindows"			=> "https://cdn1.evernote.com/scansnap/win/PFU_Scanner_Installer_b19.exe"
+	
+);
+
+//Reserve the legacy Android filename per echan
+$alldownloads['AndroidYinxiang'] = $alldownloads['AndroidYinxiangCom'];
+?>
